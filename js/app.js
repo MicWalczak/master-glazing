066e9a40df83dc783d0d8e4d7ee76d2bccff3a3d
MicWalczak <micwal2500@gmail.com>
2024-02-10 15:37:18 +0100

    chyba done

---
+++ js/app.js
@@ -0,0 +1,28 @@
+function togglePopupCall(){
+    document.getElementById("popup-1").classList.toggle('active');
+}
+function togglePopupMail(){
+    document.getElementById("popup-2").classList.toggle('active');
+}
+
+window.addEventListener("scroll",function(){
+    var header = document.querySelector('header')
+    header.classList.toggle("sticky",this.window.scrollY > 0)
+})
+
+const navLinks = document.querySelectorAll('.nav_link')
+const sections = document.querySelectorAll('.section')
+let currentSection = 'home'
+window.addEventListener('scroll',()=>{
+    sections.forEach(section=>{
+        if(window.scrollY>=section.offsetTop){
+            currentSection = section.id
+        }
+    })
+    navLinks.forEach(navLink =>{
+        if(navLink.href.includes(currentSection)){
+            navLink.classList.add('active')
+        }
+        else navLink.classList.remove('active')
+    })
+})
